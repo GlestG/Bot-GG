42dd2a8a2ac74d00cb00580594ba5385cd405d11
GlestG <57585425+GlestG@users.noreply.github.com>
2019-11-10 11:53:10 +0300

    Initial commit

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://media.giphy.com/media/eSQFIjNhmeX28/giphy.gif Selamlar,Değerli **' + username + '** Felix Sunucusuna Hoşgeldin :kissing_heart: Kurallara Bakmayı Unutmayın :thinking: İyi Roller Dostum :)');
+
+};
